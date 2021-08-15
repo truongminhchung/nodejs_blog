353e03044ff77f98b310d22bba155cdd886f5b57
TMC <minhchung811@gmail.com>
2021-08-15 21:55:53 +0700

    save

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -13,7 +15,7 @@ app.engine('hbs', handlebars({
     extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 
 app.get('/', (req, res) => {
